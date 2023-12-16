a0780c8bd921f255960afe59442b2daec802b6ee
Mark Nenadov <marknenadov@gmail.com>
2023-12-16 07:55:37 -0500

    accessibility fixes

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -11,6 +11,8 @@ export const Tab = ({label, isActive, clickHook}: TabProps) => {
     <div 
     className={`cursor-pointer mb-2 p-3 ${isActive ? "bg-sky-300" : "bg-sky-200"} w-1/3 text-center border border-1 border-black text-lg md:text-2xl`}
     onClick={clickHook}
+    role={"button"}
+    aria-label={label}
   >
     { label }
   </div>
