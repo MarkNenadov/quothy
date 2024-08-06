1138f73885b2b7b2c8c60b2332dc2e2e99ec6891
Mark Nenadov <marknenadov@gmail.com>
2024-08-06 18:04:38 -0400

    oops.. accidentially committed some testing code

--- src/components/author/AuthorQuotes.tsx
+++ src/components/author/AuthorQuotes.tsx
@@ -6,9 +6,8 @@ interface AuthorQuotesProps {
     author: string, 
     quotes: Quote[]
 }
-const quotes: Quote[] = []
 
-export const AuthorQuotes = ({ author }: AuthorQuotesProps) => (
+export const AuthorQuotes = ({quotes, author }: AuthorQuotesProps) => (
     <div className="m-1 md:m-5">
         <h2>{author}</h2>
         { (!quotes || quotes.length === 0 ) ? <p>No quotes available.</p> : null }
