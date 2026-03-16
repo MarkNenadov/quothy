e2975358618631afd4712731bf708be0d9eba6f8
Mark Nenadov <marknenadov@gmail.com>
2026-03-16 16:32:33 -0400

    add quote

--- src/data/quoteData.ts
+++ src/data/quoteData.ts
@@ -314,6 +314,14 @@ export const data: Quote[] = [
       tags: ["music", "1960s"]
     },
     {
+      quote: "Sin is not simply breaking God's law, it is breaking God's heart.",
+      book: {
+        author: "Tim Chester",
+        title: "Hosea: The Passion of God"
+      },
+      tags: ["hosea", "old-testament", "bibleß"]
+    },
+    {
       quote: "The relationships between plants and mycorrhizal fungi are key to understanding how ecosystems work...I became dizzy when I thought about what was going on underground. Plants and mycorrhizal fungi are promiscuous: many fungi can live within the roots of a single plant and many plants can connect with a single fungal network. In this way a variety of substances, from nutrients to signaling compounds, can pass between plants via fungal connnections. In simple terms, plants are socially networked by fungi. This is what is mean by the 'wood wide web.'",
       book: {
         author: "Merlin Sheldrake",
